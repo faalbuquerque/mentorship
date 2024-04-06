7b8b008b54f48870d532c8dc788755eaf7b35111
Jessica Pinheiro <jessica.pinheiro@codeminer42.com>
2024-04-06 16:57:39 -0300

    chore: add skill relationship to mentor model

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,4 +1,7 @@
 class Mentor < ApplicationRecord
+  has_many :mentor_skills
+  has_many :skills, through :mentor_skills
+
   validates :email, presence: true, uniqueness: true
 
   validates :name, presence: true
